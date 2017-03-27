1602168f5f84545a21add9316bcb04e989368856
willxm <chenweimin74@gmail.com>
2017-03-27 23:17:15 +0800

    remove some struct

--- pay/model.go
+++ pay/model.go
@@ -52,40 +52,6 @@ type BaseResult struct {
 	ThirdDiscount int64  // 第三方优惠
 }
 
-type AliWebPayResult struct {
-	NotifyTime       string `json:"notify_time"`
-	NotifyType       string `json:"notify_type"`
-	NotifyID         string `json:"notify_id"`
-	AppId            string `json:"app_id"`
-	Charset          string `json:"charset"`
-	Version          string `json:"version"`
-	SignType         string `json:"sign_type"`
-	Sign             string `json:"sign"`
-	TradeNum         string `json:"trade_no"`
-	OutTradeNum      string `json:"out_trade_no"`
-	OutBizNo         string `json:"out_biz_no"`
-	BuyerID          string `json:"buyer_id"`
-	buyerPayAmount   string `json:"buyer_pay_amount"`
-	RefundFee        string `json:"refund_fee"`
-	Subject          string `json:"subject"`
-	PayMentType      string `json:"payment_type"`
-	GmtPayMent       string `json:"gmt_payment"`
-	GmtClose         string `json:"gmt_close"`
-	SellerEmail      string `json:"seller_email"`
-	BuyerEmail       string `json:"buyer_email"`
-	SellerID         string `json:"seller_id"`
-	Price            string `json:"price"`
-	TotalFee         string `json:"total_fee"`
-	Quantity         string `json:"quantity"`
-	Body             string `json:"body"`
-	Discount         string `json:"discount"`
-	IsTotalFeeAdjust string `json:"is_total_fee_adjust"`
-	UseCoupon        string `json:"use_coupon"`
-	RefundStatus     string `json:"refund_status"`
-	GmtRefund        string `json:"gmt_refund"`
-	AliQueryResult
-}
-
 type AliQueryResult struct {
 	TradeNo        string `json:"trade_no"`
 	OutTradeNo     string `json:"out_trade_no"`
